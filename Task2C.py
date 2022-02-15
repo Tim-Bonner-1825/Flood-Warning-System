d3a7383b052362d457bd783aa42d9477eb032c6d
Nikko10Have <tenhavenikko48@gmail.com>
2022-02-15 14:12:11 +0000

    Task2C first edition

---
+++ Task2C.py
@@ -0,0 +1,20 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.station import MonitoringStation
+from floodsystem.flood import stations_highest_rel_level
+
+
+def run():
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    highestLevels = stations_highest_rel_level(stations, 10)
+    for st in highestLevels:
+        print(st[0].name, st[1])
+
+
+if __name__ == "__main__":
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -9,3 +9,13 @@ def stations_level_over_threshold(stations, tol):
                 stat_level.append((stat, stat.relative_water_level()))
     stat_level.sort(key=lambda x: x[1], reverse=True)
     return stat_level
+
+
+def stations_highest_rel_level(stations, N):
+    stats = stations_level_over_threshold(stations, 0)
+    output = []
+    if N > len(stats):
+        N = len(stats)
+    for i in range(0, N):
+        output.append(stats[i])
+    return output
