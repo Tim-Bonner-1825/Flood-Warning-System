e7430fdfcbea228041bd3eec0d611dd08e82196e
Nikko10Have <tenhavenikko48@gmail.com>
2022-02-15 11:30:36 +0000

    Fix to Task1A

--- Task1B.py
+++ Task1B.py
@@ -12,7 +12,7 @@ def run():
     stortedStations = stations_by_distance(stations, (52.2053, 0.1218))
     newStats = []
     for s in stortedStations:
-        newStats.append((s[0].name, s[0].town, s[1], 6))
+        newStats.append((s[0].name, s[0].town, s[1]))
 
     # Print output
     print("10 closest stations:")
@@ -20,8 +20,8 @@ def run():
         print(newStats[x])
 
     print("\n 10 furthest stations:")
-    for y in range(11):
-        print(newStats[-y])
+    for x in range(-11, -1):
+        print(newStats[x])
 
 
 if __name__ == "__main__":
