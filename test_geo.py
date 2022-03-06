a9bbb16a71bfc872e8e6073c0f1c47972c0719a1
Tim-Bonner-1825 <tjb99@cam.ac.uk>
2022-03-06 19:09:27 +0000

    Tests

--- test_geo.py
+++ test_geo.py
@@ -1,6 +1,7 @@
 from tokenize import String
 from typing import List, Set, Tuple
 from floodsystem.geo import *
+from floodsystem.station import MonitoringStation
 from floodsystem.stationdata import build_station_list
 import random
 
@@ -99,3 +100,37 @@ def test_rivers_by_station_number():
         assert X[n + 1][1] <= X[n][1]
     # List is of length N or less
     assert len(X) <= N
+
+
+def test_rivers_by_station_number():
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "station"
+    coord = (1, 1)
+    trange = None
+    river1 = "River 1"
+    river2 = "River 2"
+    river3 = "River 3"
+    river4 = "River 4"
+    town = "Town"
+    s1 = MonitoringStation(s_id, m_id, label, coord, trange, river1, town)
+    s2 = MonitoringStation(s_id, m_id, label, coord, trange, river2, town)
+    s3 = MonitoringStation(s_id, m_id, label, coord, trange, river1, town)
+    s4 = MonitoringStation(s_id, m_id, label, coord, trange, river3, town)
+    s5 = MonitoringStation(s_id, m_id, label, coord, trange, river2, town)
+    s6 = MonitoringStation(s_id, m_id, label, coord, trange, river1, town)
+    s7 = MonitoringStation(s_id, m_id, label, coord, trange, river3, town)
+    s8 = MonitoringStation(s_id, m_id, label, coord, trange, river4, town)
+    stations = [s1, s2, s3, s4, s5, s6, s7, s8]
+
+    assert len(rivers_by_station_number(stations, 1)) == 1
+
+    river_list = rivers_by_station_number(stations, 2)
+
+    # Multiple entries with same number in Nth position
+    assert len(river_list) == 3
+
+    assert "River 4" not in [item[0] for item in river_list]
+
+    for river in ["River 1", "River 2", "River 3"]:
+        assert river in [item[0] for item in river_list]
